2e365784269c7d8e5c10e63f9b07ed8df059b3a6
lordevito <lollo.devito@hotmail.com>
2024-02-01 22:29:07 +0100

    feat: milestone 1 (migration, resource  controller e model)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Template</title>
     <link rel="stylesheet" href="../scss/app.scss">
-    <!-- Includiamo gli assets con la direttiva @vite -->
-    @vite('resources/js/app.js')
+    <title></title>
 </head>
 <body>
     
